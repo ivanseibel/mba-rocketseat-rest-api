411985eb6fa13d08f1f3c3b9744ab6106070c78c
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-02-04 20:40:40 +0000

    Implement transaction retrieval endpoint and log transactions over 5000

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,27 @@
+import crypto from "node:crypto";
 import Fastify from "fastify";
+import { knex } from "./database";
 
 const app = Fastify();
 
-app.get("/hello", async () => {
-	return "Hello World!!";
+app.get("/hello", async (request, reply) => {
+	// const transaction = await knex("transactions")
+	// 	.insert({
+	// 		id: crypto.randomUUID(),
+	// 		title: "Salary",
+	// 		amount: Math.floor(Math.random() * 10000),
+	// 	})
+	// 	.returning("*");
+
+	// console.log(transaction);
+
+	const transactions = await knex("transactions")
+		.where("amount", ">", 5000)
+		.select("*");
+
+	console.log(transactions);
+
+	return transactions;
 });
 
 app.listen({ port: 3333 }).then(() => {
